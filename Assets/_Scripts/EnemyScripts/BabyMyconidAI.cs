5acf577da9008eb0286737412b1e922d9ba57c1a
Filip Tušek <tusek.f@gmail.com>
2017-11-09 00:54:32 +0100

    --

--- Assets/_Scripts/EnemyScripts/BabyMyconidAI.cs
+++ Assets/_Scripts/EnemyScripts/BabyMyconidAI.cs
@@ -33,8 +33,6 @@ public class BabyMyconidAI : MonoBehaviour
     private NavMeshAgent _agent;
     private Animator _myconidAC;
 
-    public Vector3 destination;
-
     private void Start ( )
     {
         Player = GameObject.Find ("Player").transform;
@@ -47,7 +45,76 @@ public class BabyMyconidAI : MonoBehaviour
     {        
         _distanceToPlayer = Vector3.Distance (transform.position, Player.position);
         _directionToPlayer = (Player.transform.position - (transform.position + transform.up));
+<<<<<<< HEAD
                 
+=======
+
+        if(_distanceToPlayer <= MeleeAttackRange && !IsAttacking)
+        {            
+            _myconidAC.SetTrigger ("Attack");
+            IsAttacking = true;
+        }
+
+        if (_distanceToPlayer <= RangedChaseRange)
+        {
+            if (_distanceToPlayer <= RangedAttackRange)
+            {
+                if(_distanceToPlayer <= FleeRange)
+                {
+                    if(_distanceToPlayer <= MeleeChaseRange)
+                    {
+                        if (_playerDetected)
+                        {
+                            _agent.isStopped = false;
+                            _agent.SetDestination (Player.position);
+                        }
+                    }
+                    else
+                    {
+                        Vector3 fleePosition = transform.position + Player.forward * FleeRangeModifier;
+
+                        NavMeshHit hit;
+
+                        NavMesh.SamplePosition (fleePosition, out hit, FleeRangeModifier, 1 << NavMesh.GetAreaFromName ("Walkable"));
+
+                        _agent.isStopped = false;
+                        _agent.SetDestination (hit.position);
+                    }                   
+                }
+                else
+                {
+                    if (!_playerDetected)
+                    {
+                        _playerDetected = true;
+                    }
+                    else
+                    {
+                        if (!IsAttacking)
+                        {
+                            _agent.isStopped = true;
+                            _myconidAC.SetTrigger ("RangedAttack");
+                            IsAttacking = true;
+                        }
+
+                        transform.rotation = Quaternion.LookRotation (_directionToPlayer);
+                    }
+                }              
+            }
+            else
+            {
+                if(_playerDetected)
+                {
+                    _agent.isStopped = false;
+                    _agent.SetDestination (Player.position);
+                }
+            }
+        }
+        else
+        {
+            _playerDetected = false;
+        }        
+
+>>>>>>> parent of 405ee07... Plugins added
         if(_agent.velocity.magnitude > 0.0f)
         {
             _myconidAC.SetBool ("IsMoving", true);
@@ -57,36 +124,36 @@ public class BabyMyconidAI : MonoBehaviour
             _myconidAC.SetBool ("IsMoving", false);
         }
 
-        //if(_checkDistanceToDeactivator)
-        //{
-        //    _distanceToLastDeactivator = Vector3.Distance (transform.position, _lastDeactivator.ClosestPoint(transform.position));
-        //}        
+        if(_checkDistanceToDeactivator)
+        {
+            _distanceToLastDeactivator = Vector3.Distance (transform.position, _lastDeactivator.ClosestPoint(transform.position));
+        }        
 
-        //if(_lastDeactivator != null)
-        //{
-        //    if(_distanceToPlayer <= _distanceToLastDeactivator)
-        //    {
-        //        RaycastHit hit;
+        if(_lastDeactivator != null)
+        {
+            if(_distanceToPlayer <= _distanceToLastDeactivator)
+            {
+                RaycastHit hit;
 
-        //        if (Physics.Raycast (transform.position + transform.up, _directionToPlayer + Player.transform.up, out hit, RangedChaseRange))
-        //        {
-        //            Debug.DrawRay (transform.position + transform.up, _directionToPlayer + Player.transform.up);
+                if (Physics.Raycast (transform.position + transform.up, _directionToPlayer + Player.transform.up, out hit, RangedChaseRange))
+                {
+                    Debug.DrawRay (transform.position + transform.up, _directionToPlayer + Player.transform.up);
 
-        //            if (hit.collider.transform.root.gameObject.CompareTag ("Player"))
-        //            {
-        //                _watchOutForPlayer = false;
-        //                _playerDetected = true;
-        //            }
-        //        }                 
-        //    }
-        //    else if(_distanceToLastDeactivator > RangedChaseRange && _distanceToLastDeactivator < _distanceToPlayer)
-        //    {
-        //        _playerDetected = false;
-        //        _watchOutForPlayer = false;
-        //        _checkDistanceToDeactivator = false;
-        //        _lastDeactivator = null;
-        //    }
-        //}
+                    if (hit.collider.transform.root.gameObject.CompareTag ("Player"))
+                    {
+                        _watchOutForPlayer = false;
+                        _playerDetected = true;
+                    }
+                }                 
+            }
+            else if(_distanceToLastDeactivator > RangedChaseRange && _distanceToLastDeactivator < _distanceToPlayer)
+            {
+                _playerDetected = false;
+                _watchOutForPlayer = false;
+                _checkDistanceToDeactivator = false;
+                _lastDeactivator = null;
+            }
+        }
     }
 
     private void OnDrawGizmosSelected ( )
